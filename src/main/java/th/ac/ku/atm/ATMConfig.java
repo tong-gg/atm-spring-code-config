82a3a15d36ff9d3f248dd9654ed1adfd8b7a59f1
Patipan <patipan.bo@ku.th>
2021-07-22 19:53:54 +0700

    Spring IoC code-config

---
+++ src/main/java/th/ac/ku/atm/ATMConfig.java
@@ -0,0 +1,30 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// code-config
+// include config file to create object instead of new object
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+//        return new DataSourceDB(); // read data from database
+        return new DataSourceFile("customers.txt"); // read data from file
+    }
+
+    @Bean
+    public Bank bank() {
+        return new Bank("My Bank", dataSource());
+    }
+
+    @Bean
+    public ATM atm(){
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return new AtmUI(atm());
+    }
+}
